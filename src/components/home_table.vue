6eeafe89fd1aaca8925966ab15ca58483bc5198f
pony <pony8674312@gmail.com>
2021-10-04 18:17:45 +0800

    update nav bar

---
+++ src/components/home_table.vue
@@ -0,0 +1,33 @@
+<template>
+    <div></div>
+</template>
+
+
+
+
+
+<script>
+export default {
+    name: 'main_table',
+    data() {
+        return {
+            status: 1
+        }
+    },
+    props: {
+        
+    },
+
+    methods: {
+        
+    }
+}
+</script>
+
+
+
+<style scoped>
+
+
+
+</style>
