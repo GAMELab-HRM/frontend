a4c3fbc12ffe4e0ecd7a1d50bdca438ca42cf853
Eric07110904 <azsx26735546@gmail.com>
2021-10-05 17:34:27 +0800

    complete permission

---
+++ src/store/modules/auth_app.js
@@ -0,0 +1,28 @@
+const state = {
+    login_status:false,
+    login_name:""
+}
+
+const mutations = {
+    UPDATE_LOGIN_STATUS:function(state,){
+        state.login_status = true
+        
+    },
+    UPDATE_LOGIN_NAME:function(state, name){
+        state.login_name = name
+    }
+}
+
+const actions = {
+    UpdateLoginStatus:function({commit}){
+        commit("UPDATE_LOGIN_STATUS",)
+    },
+    UpdateLoginName:function({commit}, name){
+        commit("UPDATE_LOGIN_NAME", name)
+    }
+}
+
+export default {
+    state, mutations, actions,
+    namespaced: true
+}
