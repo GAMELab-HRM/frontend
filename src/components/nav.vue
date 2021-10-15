2aff7d62151d80b1c6fb308a56a193fe1b75141e
pony <pony8674312@gmail.com>
2021-10-15 21:50:04 +0800

    add raw_data in home page table，add upload button in add page，adjust login CSS

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -37,10 +37,11 @@
         <el-col :xs="9" :sm="8" :md="7" :lg="6" :xl="5">
             <div id="dr_login_container">
                 <el-dropdown @command="login">
-                <el-button id="dr_login_btn" type="info" size='medium' icon="el-icon-s-custom" round v-bind:style="login_btn"> {{ login_status }} </el-button>
+                    <el-button id="dr_login_btn" type="info" size='medium' icon="el-icon-s-custom" round v-bind:style="login_btn"> {{ login_status }} </el-button>
                     <el-dropdown-menu slot="dropdown" style="background: #606A78">
-                        <el-dropdown-item command="Dr. Ray" style='font-size: 20px; color: white'>Dr. Ray</el-dropdown-item>
-                        <el-dropdown-item command="Dr. Liang" style='font-size: 20px; color: white'>Dr. Liang</el-dropdown-item>
+                        <el-dropdown-item command="Dr. Ray" >Dr. Ray</el-dropdown-item>
+                        <!-- :style='doctor_login_style' -->
+                        <el-dropdown-item command="Dr. Liang" >Dr. Liang</el-dropdown-item>
                     </el-dropdown-menu>
                 </el-dropdown>
                 <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" center>
@@ -89,6 +90,7 @@ export default {
             activeIndex: '1-1',
             act_text_color: 'red',
             dialogVisible: false,
+            doctor_login_style: 'font-size: 20px; color: white;'
         }
     },
 
@@ -102,22 +104,22 @@ export default {
     },
     methods: {
         login: function(name) {
-            
             this.$store.dispatch("auth_app/UpdateLoginStatus")
             this.$store.dispatch("auth_app/UpdateLoginName", this.login_map[name])
             this.login_status = name
             this.dialogVisible = true
             this.login_btn.borderColor = 'lightgreen'
+            console.log(name)
         },
 
         nav_bar_active(idx, event) {
             console.log(idx)
             console.log(event)
         },
-        nav_bar_submenu_hover() {
+        test(el) {
             console.log(123)
-        }
-
+            console.log(el)
+        },
     }
 
 
@@ -160,8 +162,14 @@ el-header{
     font-size: 20px
 }
 
-::v-deep .el-dropdown-menu .el-dropdown-menu__item{
-    font-size: 20px
+.el-dropdown-menu .el-dropdown-menu__item{
+    font-size: 20px;
+    color: white;
+}
+
+.el-dropdown-menu .el-dropdown-menu__item:hover{
+    background: '#4C585C';
+    color: black;
 }
 
 </style>
