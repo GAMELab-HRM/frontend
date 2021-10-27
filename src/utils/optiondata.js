bae1f05f8dd0c234b88806c0c831ef07ceabdd10
Eric07110904 <azsx26735546@gmail.com>
2021-10-27 14:21:32 +0800

    optiondata另外存成一個js檔

---
+++ src/utils/optiondata.js
@@ -0,0 +1,55 @@
+export let ws_10_options = [
+    {
+        value: 'Absent',
+        label: 'Absent'
+    },
+    {
+        value: 'IEM',
+        label: 'IEM'
+    },
+    {
+        value: 'Fragmented',
+        label: 'Fragmented'
+    },
+    {
+        value: 'Normal',
+        label: 'Normal'
+    }
+]
+
+export let mrs_options = [
+    {
+        value: 'CR',
+        label: 'Contractile Reverse'
+    },
+    {
+        value: 'not_CR',
+        label: 'Not Contractile Reverse'
+    }
+]
+
+export let hh_options = [
+    {
+        value: 'no_hh',
+        label: 'No Hiatal hernia'
+    },
+    {
+        value: 'indet_hh',
+        label: 'Hiatal hernia indeterminant'
+    },
+    {
+        value: 'hh',
+        label: 'Hiatal hernia'
+    }
+]
+
+export let rip_options = [
+    {
+        value: 'proximal',
+        label: 'Proximal RIP'
+    }, 
+    {
+        value: 'distal',
+        label: 'Distal RIP'
+    }
+]
