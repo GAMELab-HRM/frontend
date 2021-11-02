4f8ccdae614741fec3a888a540ee8130ac5cc19b
pony <pony8674312@gmail.com>
2021-11-03 00:55:01 +0800

    delete MRS、HH vues and delete basic_test_table unuse code

--- src/router/index.js
+++ src/router/index.js
@@ -30,16 +30,6 @@ const routes = [
         component: () => import("@/views/upload_files")
       },
       {
-        path: '/MRS',
-        name: 'MRS',
-        component: () => import("@/views/MRS")
-      },
-      {
-        path: '/MRS/add',
-        name: 'MRS_add',
-        component: () => import("@/views/MRS_add")        
-      },
-      {
         path: '/RDC',
         name: 'RDC',
         component: () => import("@/views/RDC")
@@ -49,17 +39,6 @@ const routes = [
         name: 'RDC_add',
         component: () => import("@/views/RDC_add")        
       },
-      {
-        path: '/Hiatal_hernia',
-        name: 'Hiatal_hernia',
-        component: () => import("@/views/Hiatal_hernia")
-      },
-      {
-        path: '/Hiatal_hernia/add',
-        name: 'Hiatal_hernia_add',
-        component: () => import("@/views/Hiatal_hernia_add")        
-      },
-
     ]
   }
 ]
