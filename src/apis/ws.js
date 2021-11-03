398d2da040287b54d5f431a9b207b7d9a1c5ad27
Eric07110904 <azsx26735546@gmail.com>
2021-11-03 23:05:39 +0800

    完成暫存功能 & fix bugs

--- src/apis/ws.js
+++ src/apis/ws.js
@@ -8,3 +8,15 @@ export function UpdateWetSwallow(obj){
     }
     return request(config)
 }
+
+export function GetWetSwallow(record_id, d_id){
+    let config = {
+        url:'http://140.118.157.26:8000/api/v1/swallows/data'+"/"+record_id,
+        method:'GET',
+        params:{
+            doctor_id:d_id
+        }
+    }
+
+    return request(config)
+}
